8076b8f878c2e082e420179e4a123d9645486842
Altieris Souza <altmsf15@gmail.com>
2024-04-21 17:46:46 -0300

    creating responsiveness of the Dashboard component

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import Signup from "@/components/Signup/Signup"
+
+export default function SignupPage(){
+    return <>
+        <body>
+            <div className="signup">
+            <Signup></Signup> 
+            </div>
+        </body>
+    </>
+}
