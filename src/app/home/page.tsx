40606b953549d1b0b8b1d85c0005b97c8bbe69f4
Hendr3w <hendreewgustavo@gmail.com>
2024-04-21 17:00:14 -0300

    v1

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "@/components/Dashboard/Dashboard"
+
+export default function Home(){
+    return <>
+        <body>
+            <div className="home">
+            <Dashboard></Dashboard> 
+            </div>
+        </body>
+    </>
+}
