a9ad66ad68323b229f73d0c293ec63d15d91ed2f
Altieris Souza <altmsf15@gmail.com>
2024-04-22 08:49:43 -0300

    implementation of login functionality

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,10 +2,13 @@ import SignupMenu from "@/components/SignupMenu/SignupMenu"
 import Container from "@/components/ContainerAuth/ContainerAuth"
 import SigninForm from "@/components/SigninForm/SigninForm"
 import "./signin.css"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignupPage(){
     return <>
         <body style={{backgroundColor: "#121214"}}>
+        <ToastContainer />
             <div className="signin">
             <SignupMenu></SignupMenu> 
                 <Container>
